242aae3e095abdb7cb37308800d8cc58bc485bcc
Danis Mavlin <mavlindanis@gmail.com>
2023-02-26 10:55:54 +0300

    Добавьте файлы проекта.

---
+++ Lopushok/DB/ProductPartial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Lopushok.DB
+{
+    public partial class Product
+    {
+        public string Materials
+        {
+            get
+            {
+                return string.Join(", ", ProductMaterials.Select(x => x.Material.Name));
+            }
+            set { }
+        }
+
+
+        public SolidColorBrush Color => (ProductSales.Any(x => x.Sale.Date.Value.Month == DateTime.Today.Month) ?
+                    new SolidColorBrush(Colors.Transparent) : new SolidColorBrush(Colors.LightCoral));
+    }
+}
